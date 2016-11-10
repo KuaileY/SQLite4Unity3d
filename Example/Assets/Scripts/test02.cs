f6d748dc8246ea2a7d2c6ad299a80b26d2fa26fb
kuaile_yzh <97369680@qq.com>
2016-11-10 20:06:09 +0800

    linq to xml

---
+++ Example/Assets/Scripts/test02.cs
@@ -0,0 +1,40 @@
+﻿using UniRx;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class test02 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var textAsset = Resources.Load<TextAsset>("aa");
+	    var xDoc = XDocument.Parse(textAsset.text);
+
+        var users = xDoc.Element("Root").Elements();
+	    users.ToObservable()
+	        .Select(t => new
+	        {
+	            userId = t.Element("UserID").Value,
+	            userName = t.Element("UserName").Value
+	        })
+	        .Do(t =>
+	        {
+	            var output = string.Format("useId:{0},useName:{1}", t.userId, t.userName);
+	            output.print();
+	        })
+	        .Subscribe();
+	}
+	
+}
+public static class StringExtension
+{
+    public static void print(this string ss)
+    {
+        Debug.Log(ss);
+    }
+
+    public static void print(this int ii)
+    {
+        print(ii.ToString());
+    }
+}
